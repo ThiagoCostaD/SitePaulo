099cbdd0ce1ff9c6e5b4cc7b4e47eb67621d71ec
Thiago Costa <thiagoocdiniz@gmail.com>
2023-06-14 10:51:50 -0300

    criando parciais

--- sitepc/views.py
+++ sitepc/views.py
@@ -2,36 +2,36 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def manutencao(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def sobre(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def palestras(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def cursos(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def consultorias(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def sistemas(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def contato(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
 
 
 def faq(request):
-    return render(request, 'sitepc/manutencao.html')
+    return render(request, 'sitepc/pages/manutencao.html')
