86e6480c9c5aa333124a197b41ed3fb5ef7aa24f
Thiago Costa <thiagoocdiniz@gmail.com>
2023-06-15 11:34:50 -0300

    fazendo a grid

--- sitepc/views.py
+++ sitepc/views.py
@@ -2,36 +2,36 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def manutencao(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def sobre(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def palestras(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def cursos(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def consultorias(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def sistemas(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def contato(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
 
 
 def faq(request):
-    return render(request, 'sitepc/pages/manutencao.html')
+    return render(request, 'sitepc/pages/home.html')
