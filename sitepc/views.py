cec835404c851e429837b8db7ab3b722c475c475
Thiago Costa <thiagoocdiniz@gmail.com>
2023-06-19 15:40:22 -0300

    fabrica de pericias testes edivisão do css

--- sitepc/views.py
+++ sitepc/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from utils.pericias.fabrica import make_pericia
 
 
 def home(request):
-    return render(request, 'sitepc/pages/home.html')
+    return render(request, 'sitepc/pages/home.html', context={
+        'pericias': [make_pericia() for _ in range(10)],
+    })
 
 
 def pericias(request, id):
-    return render(request, 'sitepc/pages/pericias-view.html')
+    return render(request, 'sitepc/pages/pericias-view.html', context={
+        'pericia': make_pericia(),
+    })
---
+++ utils/pericias/fabrica.py
@@ -0,0 +1,28 @@
+# from inspect import signature
+from random import randint
+
+from faker import Faker
+
+
+def rand_ratio():
+    return randint(840, 900), randint(473, 573)
+
+
+fake = Faker('pt_BR')
+# print(signature(fake.random_number))
+
+
+def make_pericia():
+    return {
+        'title': fake.sentence(nb_words=6),
+        'description': fake.sentence(nb_words=12),
+
+        'cover': {
+            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
+        }
+    }
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(make_pericia())
